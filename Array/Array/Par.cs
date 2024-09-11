16c5fffb0b5396e2439633876d8e3ecdf6fcbe6b
KhanAdnan1 <ka4180910@gmail.com>
2024-09-11 12:02:42 +0530

    commit

---
+++ Array/Array/Par.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Array
+{
+    partial class Par
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+    partial class Par
+    {
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+    }
+    partial class Par
+    {
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine(a*b);
+        }
+    }
+}
---
+++ Array/Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using Array;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Arr obj = new Arr();
+        obj.Array();
+        obj.Array2();
+
+        var emp=new List<Emp>();
+        emp.Add(new Emp(03,"Sadan",02358632,12500));
+        emp.Add(new Emp(04, "Rehan", 02358632, 12500));
+        emp.Add(new Emp(05, "Faizan", 02358632, 12500));
+
+        foreach(var item in emp)
+        {
+            Console.WriteLine(item.Name);
+        }
+
+        Dict dict=new Dict();
+        dict.Dictonary();
+
+        Par par=new Par();
+        par.Add(4, 2);
+        par.Sub(7, 1);
+        par.Mul(3, 2);
+
+    }
+}
