38bbac40cab6bf0a3f37046be5db4c9dbb97a104
KhanAdnan1 <ka4180910@gmail.com>
2024-09-11 11:59:25 +0530

    commit

---
+++ FileHandling/FileHandling/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using File_Handling;
+class FileHandling
+{
+    static void Main(string[] args)
+    {
+       WriteFile Writeobj=new WriteFile();
+       Writeobj.CreatData();
+
+      ReadData Readobj=new ReadData();
+      Readobj.Read();
+
+  ;
+
+
+    }
+}
