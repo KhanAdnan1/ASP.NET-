e8785c3afd7f428d9c29ff37901157de20f5aa4e
KhanAdnan1 <ka4180910@gmail.com>
2024-09-11 12:02:07 +0530

    commit

---
+++ FileHandling/FileHandling/WriteFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Handling
+{
+    class  WriteFile
+    {
+       public void CreatData()
+        {
+            FileStream fs = new FileStream("D://File/file.txt", FileMode.Create);
+            StreamWriter sm = new StreamWriter(fs);
+            sm.WriteLine("I'M Adnan Khan");
+            Console.WriteLine("File created successfully");
+            sm.Close();
+            fs.Close();
+        }
+
+      
+
+    }
+}
